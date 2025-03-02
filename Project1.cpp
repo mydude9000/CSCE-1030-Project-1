0f78e9fa0da6319573730e6e298a2dbde86b25e6
mydude9000 <jesus.ramirez5618@gmail.com>
2025-03-01 20:57:00 -0600

    Update Project1.cpp

--- Project1.cpp
+++ Project1.cpp
@@ -50,6 +50,47 @@ int main () {
     }
     while (rand1 >= rand2);
 
+    // FIXME: Need to make sure first and last initials are capitalized 
+
+    // Welcome message for user and what to expect 
+    cout << "Welcome " << name << ".\n";
+    cout << "Two random numbers have been generated for you." << endl;
+    
+    // Score will be set to 100 and started random seed 
+    int score = 100;
+    srand(time(0));
+
+    // Generating two random numbers inclusively from 100 to 150
+    int rand1, rand2;
+    do {
+        rand1 = (rand() % 51) + 100;
+        rand2 = (rand() % 51) + 100;
+    }
+    while (rand1 >= rand2);
+
+    // Establishing an enumeration for given choices of add, subtract, display, giveup, and exit
+    int userChoice;
+
+    enum choice {ADD = 1, SUBTRACT = 2, DISPLAY = 3, GIVEUP = 4, EXIT = 5};
+
+    cout << "\n1. ADD\n2. SUBTRACT\n3. DISPLAY\n4. GIVEUP\n5. EXIT\n";
+    cout << "Enter your perferred choice: ";
+    cin >> userChoice; 
+
+    switch (userChoice) {
+        case 1:
+        int guess; 
+        cout << "Please enter your guess of the sum of random numbers: ";
+        cin >> guess;
+        int realSum = rand1 + rand2;
+        if (abs(guess - realSum) < 5) {    // Creating if statement to accept guesses that are less than 5 from realSum
+            score += 5;
+            cout << "";
+        }
+
+
+
+    }
     
     return 0; 
 }
